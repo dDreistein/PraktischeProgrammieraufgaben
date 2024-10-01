59a474d922312a2efccd9b2b8251f606ee09f4d5
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-10-01 16:48:58 +0200

    Aufg15

--- Aufg15ZahlenRaten/Program.cs
+++ Aufg15ZahlenRaten/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool exitKeyEntered = false;
+
+            while (!exitKeyEntered)
+            {
+                //Variablen zurücksetzen
+                bool solved = false;
+                int trys = 1;
+
+                int randomNumber = Random.Shared.Next(2, 101);
+
+                Console.WriteLine("Errate deine Zahl zwischen 1 und 100: ");
+
+                //Überprüffen ob das Rätzel gelöst ist.
+                while (!solved) {
+                    if (int.TryParse(Console.ReadLine(), out int guess) && guess <= 100) {
+                        if (guess > randomNumber)
+                        {
+                            Console.WriteLine("Zahl ist zu Gross! Nächster Versuch: ");
+                        }
+                        else if (guess < randomNumber)
+                        {
+                            Console.WriteLine("Zahl ist zu Klein! Nächster Versuch: ");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Die Zahl stimmt! Du hast {trys} Versuche benötigt. Noch einmal Spielen? [y/n]");
+
+                            if (Console.ReadLine() == "y") 
+                            {
+                                exitKeyEntered = false;
+                            }
+                            else
+                            {
+                                exitKeyEntered = true;
+                            }
+                            
+                            solved = true;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ungültige Eingabe.");
+                    }
+                    trys++;
+                }
+                Console.Clear();
+
+            }
         }
     }
 }
