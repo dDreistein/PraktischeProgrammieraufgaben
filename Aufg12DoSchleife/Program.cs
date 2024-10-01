3721bacd384152c10797dd4551d14e74f481538b
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-10-01 14:11:32 +0200

    Aufg12

---
+++ Aufg12DoSchleife/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i = 0;
+        Console.WriteLine("...");
+        while (i >= 0)
+        {
+            Console.WriteLine("...");
+        }
+    }
+}
