141a35f98ccf6ef20c8acf7b1e2b4bbfd6218db0
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-10-01 09:32:43 +0200

    Start

---
+++ Aufg1Addierer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufg1Addierer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Die Methode WriteLine() fügt am Ende einen Zeilenumbruch ein
+            Console.WriteLine("Dieses Programm berechnet die Summe von zwei Zahlen.");
+            Console.WriteLine();
+            // Die Methode Write() fügt KEINEN Zeilenumbruch ein
+            Console.Write("Zahl 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Zahl 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Summe: {num1 + num2}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aufg2SekundenZuMonat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufg2SekundenZuMonat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Berechnung von Sekunden eines Monats in Abhängigkeit seiner Anzahl Tage");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Wieviele Tage hat der Monat, für den Sie die Sekundenzahl berechnen wollen");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int daysOfMonth) == true)
+            {
+                if (daysOfMonth < 32)
+                {
+                    Console.WriteLine($"Ein Monat mit {daysOfMonth} tagen hat {86400 * daysOfMonth} Sekunden.");
+                }
+                else
+                {
+                    Console.WriteLine($"Ungültige Eingabe. Ein Monat mit {daysOfMonth} Tagen gibt es nicht.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Eingabe. Bitte verwenden Sie Ganzzahlen.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
