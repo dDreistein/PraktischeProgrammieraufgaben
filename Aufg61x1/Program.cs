1cedc3505c95da73ea96b6370d52ece85dce52e1
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-10-01 11:21:48 +0200

    Aufg6

---
+++ Aufg61x1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("-----------");
+        Console.WriteLine("Kleines 1x1");
+        Console.WriteLine("-----------");
+        Console.WriteLine();
+        for (int i = 1; i <= 10; i++)
+        {
+            int factor1 = i;
+            for (int i2 = 1; i2 <= 10; i2++)
+            {
+                int factor2 = i2;
+                Console.Write(factor1 * factor2 + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
