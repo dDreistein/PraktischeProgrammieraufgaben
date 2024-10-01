3de9feba7812ea35d4d36cece7282b7283cf2091
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-10-01 13:02:34 +0200

    Aufg 8

---
+++ Aufg8ZahlZuBinär/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool exitKeyEntered = false;
+        while (!exitKeyEntered)
+        {
+            Console.WriteLine("Ganzzahlige Dezimalzahl (q to Quit):");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() != "q")
+            {
+                int zahl = Convert.ToInt32(input);
+                string bin = Convert.ToString(zahl, 2);
+                Console.WriteLine($"Binär: {bin}");
+                Console.WriteLine();
+            }
+            else
+            {
+                exitKeyEntered = true;
+            }
+        }
+    }
+}
