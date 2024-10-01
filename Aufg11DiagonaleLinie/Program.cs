2f834a2fa680d24d7a3783e5135d360aafd95343
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-10-01 14:03:58 +0200

    Aufg11

---
+++ Aufg11DiagonaleLinie/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Wie lang soll die Linie sein?");
+        Console.Write("Deine Eingabe: ");
+        int lineLength = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+        for (int i = 1; i <= lineLength; i++)
+        {
+            for (int j = 1; j <= lineLength; j++)
+            {
+                if (j - i == 0)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
