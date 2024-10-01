8e4d4e992ce17d7dec184b1c05c3bb6bf4489d50
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-10-01 11:05:49 +0200

    Aufg5

---
+++ Aufg5Flussdiagramm2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Wie viele Kilometer möchtest du rennen?");
+        int kilometersToRun = Convert.ToInt32(Console.ReadLine());
+        if (kilometersToRun > 42)
+        {
+            Console.WriteLine("Das schaffst du nicht!");
+        }
+        else
+        {
+
+            int n = kilometersToRun * 1000 / 400;
+
+            Console.WriteLine($"Das sind {n} Runden. Bereit für den Lauf?");
+            Console.WriteLine("Ja/Nein");
+            if (Console.ReadLine() == "Ja")
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine($"Du läufst Runde {i}");
+                }
+                Console.WriteLine("Du hast es geschafft!");
+            }
+        }
+    }
+}
