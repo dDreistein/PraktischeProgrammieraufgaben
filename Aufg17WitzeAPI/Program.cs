b7083ecf816f72c391ca6eea707b59e6bf2ef998
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-10-02 08:54:13 +0200

    Aufg17: Started, Aufg7: fixed formating

---
+++ Aufg17WitzeAPI/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Aufg17WitzeAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exitKeyEntered = false;
+
+            while (!exitKeyEntered)
+            {
+                Console.WriteLine("...");
+                makeParagraph(1);
+                Console.Write("Nächsten Witz holen? [y/n] ");
+                exitKeyEntered = detectExitKey();
+                makeParagraph(2);
+            }
+        }
+
+        static bool detectExitKey()
+        {
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                return false;
+            }
+            else 
+            { 
+                return true;
+            }
+        }
+
+        static void makeParagraph(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Aufg7Durch3Und5Teilbar/Program.cs
+++ Aufg7Durch3Und5Teilbar/Program.cs
@@ -14,5 +14,6 @@
                 }
             }
         }
+        Console.WriteLine();
     }
 }
